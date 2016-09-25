700ff6a18b3554b3506a6e84c9c98c30c883cfff
Gus Rylander <rylander@nytimes.com>
2016-09-24 20:23:39 -0400

    managing expiring caches of noaa precip data in localStorage

--- js/noaa.js
+++ js/noaa.js
@@ -5,37 +5,72 @@ var NOAA = function() {
         TOKEN: "zuaINMzpFJTIwKVgRhCBNSUQggvWkpUX"
     }
 
+    var noaaPayload = function(dateRange) {
+        util = Util();
+        return {
+            datasetid: "GHCND",
+            stationid: "GHCND:USW00094789",
+            startdate: util.formattedDate(dateRange[0], '-'),
+            enddate: util.formattedDate(dateRange[1], '-'),
+            datatypeid: "PRCP",
+            limit: "1000",
+            includemetadata: "false",
+            units: "standard"
+        }
+    }
+
     var  getPrecipData = function(dateRange) { 
-        var util = Util(),
-            noaa_payload = {
-                datasetid: "GHCND",
-                stationid: "GHCND:USW00094789",
-                startdate: util.formattedDate(dateRange[0], '-'),
-                enddate: util.formattedDate(dateRange[1], '-'),
-                datatypeid: "PRCP",
-                limit: "1000",
-                includemetadata: "false",
-                units: "standard"
-            };
+
+        var util, noaa_payload, year, key, cache, precipData;
+
         return new Promise( function(resolve, reject) {
-            $.ajax({
-                url: NOAA["URL"],
-                type: "GET",
-                data: noaa_payload,
-                headers: {"token": NOAA["TOKEN"],},
-                beforeSend: function( xhr ) {
-                    $("#chart").html("<i id='spinner' class='fa fa-refresh fa-spin fa-3x fa-fw'></i><span class='sr-only'>Loading...</span>");
-                }
-            })
-            .done(function(data, textStatus, jqXHR) {
-                resolve(data.results);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                reject("errors:" + errorThrown)
-            })
-            .always(function() {
-                $("#chart").html("");
-            });
+
+            year = dateRange[0].getFullYear();
+            key = "noaa_precip_" + year;
+            cache = localStorage;
+
+            // try to fetch data from localStorage, fail and fetch from NOAA if cache is empty or expired.
+            try { 
+                var now, isExpired;
+
+                precipData = JSON.parse(cache.getItem(key));
+                if (! precipData) throw "" + year + " data is not cached.  Fetching from NOAA";
+
+                isExpired = d3.timeDay.offset(precipData.lastUpdate, 1) < new Date(Date.now());
+                if (isExpired) throw "" + year + " data is expired.  Re-fetching from NOAA";
+
+                resolve(precipData.data);
+
+            } catch (e) {
+
+                console.log(e);
+
+                $.ajax({
+                    url: NOAA["URL"],
+                    type: "GET",
+                    data: noaaPayload(dateRange),
+                    headers: {"token": NOAA["TOKEN"],},
+                    beforeSend: function( xhr ) {
+                        $("#chart").html("<i id='spinner' class='fa fa-refresh fa-spin fa-3x fa-fw'></i><span class='sr-only'>Loading...</span>");
+                    }
+                })
+                .done(function(precipData, textStatus, jqXHR) {
+                    precipData = {
+                        lastUpdate: Date.now(), 
+                        data: precipData.results
+                    }
+                    delete precipData.results;
+                    cache.setItem(key, JSON.stringify(precipData))
+                    resolve(precipData.data);
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    reject("errors:" + errorThrown);
+                })
+                .always(function() {
+                    $("#chart").html("");
+                });
+            }
+
         });
     }
 
